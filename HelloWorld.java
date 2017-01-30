dfa00f87e609efc774e1811c97b5b692c8c86968
vivekranjan92 <vivekranjan92@gmail.com>
2017-01-30 20:37:37 +0530

    taking out as Hello and Bye
    when number is less than 100 returns Hello and when number is greater than 100 returns Bye.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		
+		boolean IsAlean = true;
+		int myNumber = 80;
+		if (myNumber < 100);
+		System.out.println("Hello")
+
+		else (myNumber > 100);
+		System.out.println("bye-bye")		
+
+	}
+}
