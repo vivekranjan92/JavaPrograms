9a81a5915e7776852fcecf87e72b3ff6e3fb0c12
vivekranjan92 <vivekranjan92@gmail.com>
2017-02-08 17:39:16 +0530

    FindingIndexNumber
    finding index number of a character in the string

---
+++ StringTest_6.java
@@ -0,0 +1,10 @@
+public class StringTest_6
+{
+
+	public static void main(String[] args)
+	{
+
+		String myString = "My name is Vivek Ranjan Singh";
+		System.out.println(myString.indexof('R'));
+	}
+}
