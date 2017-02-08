5d00f4e6e5445ee4de23f9f2c82f0254dac2ccf3
vivekranjan92 <vivekranjan92@gmail.com>
2017-02-08 17:34:43 +0530

    LengthOfString
    code for finding out the length of the string.

---
+++ StringTest_2.java
@@ -0,0 +1,9 @@
+public class StringTest_1 {
+
+	public static void main(String[] args) {
+		String myString = "My Name Is Vivek Ranjan Singh";
+		int myStringLength = myString.length();
+		System.out.println(myStringLength);
+
+	}
+}
