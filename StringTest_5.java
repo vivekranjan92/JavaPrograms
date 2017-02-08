f3f2fd1efbfd779244ed0f68ccd711884836a291
vivekranjan92 <vivekranjan92@gmail.com>
2017-02-08 17:38:02 +0530

    ReplacingCharacter
    replacing a character with another

---
+++ StringTest_5.java
@@ -0,0 +1,10 @@
+public class StringTest_5
+{
+
+	public static void main(String[] args)
+	{
+
+		String myString = "My name Is Vivek Ranjan Singh";
+		System.out.println(myString.replace('n','p'));
+	}
+}
