740a27a8a124ac25da698b8c25b34172b0345358
vivekranjan92 <vivekranjan92@gmail.com>
2017-02-08 17:36:04 +0530

    ChangingToLowercase
    changing the string to lowercase

---
+++ StringTest_3.java
@@ -0,0 +1,12 @@
+public class StringTest_3 {
+
+	public static void main(String[] args) {
+		
+
+
+		String myString = "My Name Is Vivek Ranjan Singh";		
+		String myStringinLowerCase = myString.toLowerCase();
+		System.out.println(myStringinLowerCase)
+
+	}
+}
